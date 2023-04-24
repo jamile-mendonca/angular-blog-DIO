a238e3e9a35c851a5aa0fb4e8cfa5478ec62fa99
Jamile-Xavier <jamile.x.m@gmail.com>
2023-04-24 11:05:21 -0300

    Create components styles

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { withLatestFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.css']
+})
+export class MenuTitleComponent {
+
+}
+
+
